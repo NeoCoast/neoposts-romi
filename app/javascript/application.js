94016de3c87190e84fea07ddbf9710c4887c7c74
Romina Piria <114534563+asxasy@users.noreply.github.com>
2024-06-12 10:26:08 -0300

    Follow_data_on_user_profile (#12)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,3 +8,39 @@
 import Rails from '@rails/ujs'
 import './add_jquery';
 Rails.start();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var followingClicked = false;
+    var followersClicked = false;
+  
+    document.getElementById('following-count').addEventListener('click', function() {
+      if (!followingClicked) {
+        document.querySelector('.following-list').classList.remove('hidden');
+        document.querySelector('.followers-list').classList.add('hidden');
+        document.getElementById('users-posts').classList.add('hidden');
+        followingClicked = true;
+        followersClicked = false;
+      } else {
+        document.querySelector('.following-list').classList.add('hidden');
+        document.querySelector('.followers-list').classList.add('hidden');
+        document.getElementById('users-posts').classList.remove('hidden');
+        followingClicked = false;
+      }
+    });
+
+  document.getElementById('followers-count').addEventListener('click', function() {
+    if (!followersClicked) {
+      document.querySelector('.followers-list').classList.remove('hidden');
+      document.querySelector('.following-list').classList.add('hidden');
+      document.getElementById('users-posts').classList.add('hidden');
+      followersClicked = true;
+      followingClicked = false;
+    } else {
+      document.querySelector('.followers-list').classList.add('hidden');
+      document.querySelector('.following-list').classList.add('hidden');
+      document.getElementById('users-posts').classList.remove('hidden');
+      followersClicked = false;
+    }
+  });
+});
+
